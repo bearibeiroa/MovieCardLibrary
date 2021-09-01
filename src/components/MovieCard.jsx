aea3a68e2d30fb86e59b1d9464bc892d922ab923
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-09-01 21:17:36 +0100

    Adiciona CSS e finaliza projeto

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,15 +6,15 @@ class MovieCard extends React.Component {
   render() {
     const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
     return (
-      <section>
-        <div>
-          <img src={ imagePath } alt={ title } />
+      <section className="movie-card">
+        <div className="movie-card-body">
+          <img className="movie-card-image" src={ imagePath } alt={ title } />
           <div>
-            <h4>{ title }</h4>
-            <h5>{ subtitle }</h5>
-            <p>{ storyline }</p>
+            <h4 className="movie-card-title">{ title }</h4>
+            <h5 className="movie-card-subtitle">{ subtitle }</h5>
+            <p className="movie-card-storyline">{ storyline }</p>
           </div>
-          <Rating rating={ rating } />
+          <Rating className="rating" rating={ rating } />
         </div>
       </section>
     );
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -6,7 +6,7 @@ class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
     return (
-      <div>
+      <div className="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div>
     );
