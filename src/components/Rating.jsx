ad2a9b808ef55ea54dfc0e30f2454499016ad7c9
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-09-01 21:05:31 +0100

    Ajusta lint no rating e define propTypes do mesmo

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
     const { rating } = this.props;
     return (
       <div className="movie-card-rating">
-        <span className="rating"> { rating }</span>
+        <span className="rating">
+          {rating}
+        </span>
       </div>
     );
   }
 }
+
+Rating.propTypes = {
+  rating: PropTypes.number,
+};
+
+Rating.defaultProps = {
+  rating: null,
+};
 
 export default Rating;
